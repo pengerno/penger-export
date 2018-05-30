5310bbb3c0090eb5a49e8c99a6ffcb29f036a27b
Kjetil Mehl <kjetil.mehl@schibsted.no>
2018-05-30 12:26:14 +0200

    Adds forsikringspakke example

--- src/test/scala/no/penger/export/JsonTests.scala
+++ src/test/scala/no/penger/export/JsonTests.scala
@@ -18,6 +18,10 @@ class JsonTests extends FunSuite with TestHelpers {
     assertJson[Utleieforsikring](resourceToString("/utleieforsikring-example.json"))
   }
 
+  test("ser-des Forsikringspakke") {
+    assertJson[Forsikringspakke](resourceToString("/forsikringspakke-example.json"))
+  }
+
   private def assertJson[T](source: String)(implicit s: SchemaFor[T], f: FromRecord[T], t: ToRecord[T]): Unit = {
     val entity       = jsonToEntity[T](source).right.get
     val json         = entityToJson[T](entity)
